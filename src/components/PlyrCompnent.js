83736f9d30f1d8cb0abc0374ead18f413d2ea235
csenazmul96 <csenazmul96@gmail.com>
2025-08-04 18:07:49 +0600

    Add Plyr video player and integrate TailwindCSS
    Introduced the Plyr video player with custom Timer functionality using React. Replaced custom CSS with TailwindCSS setup for styling, updated dependencies, and cleaned up unused code and assets for a simpler application structure.

---
+++ src/components/PlyrCompnent.js
@@ -0,0 +1,85 @@
+'use client';
+
+import React, {forwardRef, useEffect, useImperativeHandle, useRef, useState} from 'react';
+import Plyr from 'plyr-react'; 
+
+
+const TimerComponent = forwardRef(function TimerComponent({ }, ref) {
+    const [time, setTime] = useState(0);
+    useImperativeHandle(ref, () => {
+        return {
+            setNewTime(sec) {
+                const minutes = Math.floor(sec / 60);
+                const seconds = Math.floor(sec % 60);
+                const formatted = `${String(minutes).padStart(2, '0')}:${String(seconds).padStart(2, '0')}`;
+                setTime(formatted);
+            },
+        };
+    }, []);
+
+    return (
+        <h1 ref={ref}>{time}</h1>
+    );
+});
+
+
+export default function PlyrCompnent() {
+
+    const playerRef = useRef(null);
+    const timerRef = useRef(null);
+    useEffect(() => {
+        if (playerRef.current?.plyr) {
+            setTimeout(() => {
+            const plyrInstance = playerRef.current.plyr;
+                plyrInstance.on('timeupdate', (e)=>{
+                    timerRef.current.setNewTime(plyrInstance.currentTime)
+                })
+
+            }, 50)
+        }
+    }, []);
+
+    const videoSource = {
+        type: "video",
+        sources: [
+            {
+                src: "https://cdn.plyr.io/static/demo/View_From_A_Blue_Moon_Trailer-576p.mp4",
+                type: "video/mp4",
+            }
+        ],
+        tracks: [
+            {
+                kind: 'subtitles',
+                label: 'English',
+                srcLang: 'en',
+                src: '/WebVTT.vtt',
+                default: true
+            }
+        ],
+    };
+
+    return (
+        <div className="relative player-wrapper">
+            <TimerComponent ref={timerRef} />
+            <Plyr
+                ref={playerRef}
+                source={videoSource}
+                crossOrigin="anonymous"
+                options={{
+                    controls: [
+                        'play-large',
+                        'play',
+                        'progress',
+                        'mute',
+                        'volume',
+                        'captions',
+                        'settings',
+                        'fullscreen',
+                    ]
+                }}
+            />
+        </div>
+    )
+}
+
+
